7221dc15acb361ddc1e96d60e5bfb8fd170d287d
paul.frost <paul.frost@talktalk.net>
2022-04-15 10:52:25 +0100

    removed pystache

--- TPAM/settings.py
+++ TPAM/settings.py
@@ -19,8 +19,6 @@ print('BASE_DIR = ', BASE_DIR)
 DATAIO_DIR = os.path.join("D:\\Data", "TPAM")
 DEFAULT_AUTO_FIELD='django.db.models.AutoField' #Required as from Django 3.2
 
-GDAL_LIBRARY_PATH = r'C:\\OSGeo4W64\\bin\\gdal301'
-
 # https://docs.djangoproject.com/en/3.1/ref/settings/#std:setting-INSTALLED_APPS
 INSTALLED_APPS = [
 
